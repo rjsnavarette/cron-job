dd90def0a7cb45ffb97ac2b0c313181dd1cc032b
Raven <rhold.navarette@kodakollectiv.com>
2021-03-17 14:50:23 +0800

    Configure the schedule rb

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,2 +1,6 @@
 class Bill < ApplicationRecord
+  def self.delete_old
+    @old_invoices = Bill.where('data < ?', 30.days.ago)
+    @old_invoices.delete_all
+  end
 end
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,8 @@
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+env :MAILTO, "root"
+env :PATH, "/Users/kk48/.rvm/gems/ruby-3.0.0/gems/whenever-1.0.0/bin"
+env :RVM_VERSION, "1.29.12"
+set :environment, :development
+
+every 2.minutes do
+  runner "Bill.delete_old", :output => {:error => 'error.log', :standard => 'cron.log'}
+end
